a280b127d8152f7a107f7230a8fac4552b86dbed
dmkolosov <maker.bug.00@inbox.ru>
2020-10-25 09:42:00 +0300

    feat: implement check funtion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
+    let pairsOfBrackets = Object.fromEntries(bracketsConfig);
+    let stack = [];
+    let elements = str.split("");
 
+    for (let element of elements) {
+        console.log(stack)
+        if (stack[stack.length - 1] === element) {
+            console.log(element,'pop')
+            stack.pop();
+        } else if (pairsOfBrackets[element]) {
+            console.log(element,'push',pairsOfBrackets[element])
+            stack.push(pairsOfBrackets[element]);
+        } else {
+            console.log('return false')
+            return false;
+        }
+    }
+console.log('stack',stack,stack.length)
+    return stack.length === 0;
 };
