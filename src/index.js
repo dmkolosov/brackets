5ab16c9525dd321a641774df2501e76308b7831b
dmkolosov <maker.bug.00@inbox.ru>
2020-10-25 09:42:50 +0300

    refactor:  delete log

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ module.exports = function check(str, bracketsConfig) {
     let elements = str.split("");
 
     for (let element of elements) {
-        console.log(stack)
         if (stack[stack.length - 1] === element) {
-            console.log(element,'pop')
             stack.pop();
         } else if (pairsOfBrackets[element]) {
-            console.log(element,'push',pairsOfBrackets[element])
             stack.push(pairsOfBrackets[element]);
         } else {
-            console.log('return false')
             return false;
         }
     }
-console.log('stack',stack,stack.length)
+
     return stack.length === 0;
 };
